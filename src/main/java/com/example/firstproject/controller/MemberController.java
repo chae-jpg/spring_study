9cb90419f2fa9216a78a66cfe8247f15bacb37cd
cakenpeace <chwon02@naver.com>
2024-01-10 01:47:47 +0900

    Chap 3

---
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -0,0 +1,31 @@
+package com.example.firstproject.controller;
+
+import com.example.firstproject.dto.MemberForm;
+import com.example.firstproject.entity.Member;
+import com.example.firstproject.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MemberController {
+    @Autowired
+    private MemberRepository memberRepository;
+    @GetMapping("/signup")
+    public String newMemberForm() {
+        return "/members/new";
+    }
+
+    @PostMapping("/join")
+    public String createMember(MemberForm form) {
+        System.out.println(form.toString());
+        //1. DTO 엔티티로 변환
+        Member member = form.toEntity();
+        System.out.println(member.toString());
+        //2. 엔티티 레포지토리에 저장
+        Member saved = memberRepository.save(member);
+        System.out.println(saved.toString());
+        return "";
+    }
+}
